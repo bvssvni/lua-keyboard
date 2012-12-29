376d153ef1c2b1fd11dcff7372cdd4df5e8923c7
Sven Nilsen <sven@cutoutpro.com>
2012-12-29 10:41:32 +0100

    Updated description.

--- keyboard.lua
+++ keyboard.lua
@@ -12,6 +12,9 @@ Modifier keys can be troublesome to deal with in keypress events.
 For example, the user hits Ctrl and then another key, which state needs to be tracked.
 This module keeps track of which modifier keys are pressed.
 
+The events must be sent manually using 'keyboard.press' and 'keyboard.release'.
+If the 'keyboard.press(key)' returns true, then it should be ignored as input for text.
+
 --]]
 
 --[[
